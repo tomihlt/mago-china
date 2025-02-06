5c8c5ce52f64aeea17ea254b653fca1e56acb300
Tomas Haulet <tomshaulet@gmail.com>
2025-02-06 15:52:08 -0300

    interfaz editada

--- app/imageDetailScreen.tsx
+++ app/imageDetailScreen.tsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
-import { useLocalSearchParams, useRouter } from 'expo-router';
-import { Animated } from 'react-native';
+import { View, Text, Image, StyleSheet, ScrollView, Alert } from 'react-native';
+import { useLocalSearchParams, useNavigation, useRouter } from 'expo-router';
 import { Pressable } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { Alert } from 'react-native';
 import { deleteImage } from '@/services/dataController';
 
 type FormData = {
@@ -19,27 +17,28 @@ type FormData = {
 };
 
 export default function ImageDetailScreen() {
-  // Efectos
-  const [isPressedD, setIsPressedD] = useState(false); // Estado para saber si el botón de reset está presionado
-  const [isPressedA, setIsPressedA] = useState(false); // Estado para saber si el botón de aceptar está presionado
+  const [isPressedD, setIsPressedD] = useState(false);
+  const [isPressedA, setIsPressedA] = useState(false);
 
   const { image, form, objHash } = useLocalSearchParams();
   const router = useRouter();
+  const navigation = useNavigation();
 
-  // Asegurar que form es un string
-  const formString = Array.isArray(form) ? form[0] : form;  
+  const formString = Array.isArray(form) ? form[0] : form;
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: false, // Oculta el header
+    });
+  }, [navigation]);
+
 
   if (!image || !formString || !objHash) {
     return <Text>Los datos de la imagen no están disponibles.</Text>;
   }
 
-  // const imageUri = Array.isArray(image) ? image[0] : image;
   const imageUri = encodeURI(Array.isArray(image) ? image[0] : image);
   const parsedForm: FormData = JSON.parse(formString);
-  console.log('image:', image);
-  console.log('form:', form);
-  console.log('objHash:', objHash);
-
 
   const handleDelete = () => {
     Alert.alert(
@@ -48,64 +47,169 @@ export default function ImageDetailScreen() {
       [
         {
           text: "Cancelar",
-          style: "cancel"
+          style: "cancel",
         },
-        { 
-          text: "Eliminar", 
+        {
+          text: "Eliminar",
           onPress: () => {
             deleteImage(objHash as string)
               .then(() => {
-                // Aquí puedes agregar lógica adicional después de eliminar la imagen, como navegar a otra pantalla
                 console.log('Imagen eliminada exitosamente');
                 router.back();
               })
               .catch((error) => {
                 console.error('Error al eliminar la imagen:', error);
               });
-          }
-        }
+          },
+        },
       ]
     );
   };
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <Image source={{ uri: imageUri }} style={styles.image} />
-      {/* <Animated.Image source={{ uri: imageUri }} style={styles.image} /> */}
-      <Text style={styles.title}>Detalles de la Imagen</Text>
-      <Text style={styles.text}>Nombre Proveedor: {parsedForm.nombreProveedor}</Text>
-      <Text style={styles.text}>Código: {parsedForm.codigo}</Text>
-      <Text style={styles.text}>Descripción: {parsedForm.descripcion}</Text>
-      <Text style={styles.text}>Precio: {parsedForm.precio}</Text>
-      <Text style={styles.text}>Cantidad por Bulto: {parsedForm.cantidadBulto}</Text>
-      <Text style={styles.text}>Cubicaje: {parsedForm.cubicaje}</Text>
-      <Text style={styles.text}>Peso: {parsedForm.peso}</Text>
-      <Text style={styles.text}>Observaciones: {parsedForm.obs}</Text>
+      <View style={styles.card}>
+        <Image source={{ uri: imageUri }} style={styles.image} />
+        <Text style={styles.title}>Detalles de la Imagen</Text>
+        <View style={styles.detailsContainer}>
+          <Text style={styles.label}>Nombre Proveedor:</Text>
+          <Text style={styles.value}>{parsedForm.nombreProveedor}</Text>
+
+          <Text style={styles.label}>Código:</Text>
+          <Text style={styles.value}>{parsedForm.codigo}</Text>
+
+          <Text style={styles.label}>Descripción:</Text>
+          <Text style={styles.value}>{parsedForm.descripcion}</Text>
+
+          <Text style={styles.label}>Precio:</Text>
+          <Text style={styles.value}>{parsedForm.precio}</Text>
+
+          <Text style={styles.label}>Cantidad por Bulto:</Text>
+          <Text style={styles.value}>{parsedForm.cantidadBulto}</Text>
+
+          <Text style={styles.label}>Cubicaje:</Text>
+          <Text style={styles.value}>{parsedForm.cubicaje}</Text>
+
+          <Text style={styles.label}>Peso:</Text>
+          <Text style={styles.value}>{parsedForm.peso}</Text>
+
+          <Text style={styles.label}>Observaciones:</Text>
+          <Text style={styles.value}>{parsedForm.obs}</Text>
+        </View>
+      </View>
 
       {/* Botones */}
       <View style={styles.buttonContainer}>
-          <Pressable style={[styles.deleteButton, isPressedD && styles.buttonIsPressed]} onPress={handleDelete} onPressIn={() => setIsPressedD(true)} onPressOut={() => setIsPressedD(false)}>
-            <Ionicons name="trash" size={20} color="rgb(252, 59, 59)" />
-            <Text style={styles.buttonTextDelete}>Eliminar</Text>
-          </Pressable>
-          <Pressable style={[styles.acceptButton, isPressedA && styles.buttonIsPressed]} onPress={() => router.back()} onPressIn={() => setIsPressedA(true)} onPressOut={() => setIsPressedA(false)}>
-            <Ionicons name="checkmark" size={20} color="rgb(81, 196, 71)" />
-            <Text style={styles.buttonTextAccept}>Aceptar</Text>
-          </Pressable>
-        </View>
+        <Pressable
+          style={({ pressed }) => [
+            styles.deleteButton,
+            pressed && styles.buttonIsPressed,
+          ]}
+          onPress={handleDelete}
+          onPressIn={() => setIsPressedD(true)}
+          onPressOut={() => setIsPressedD(false)}
+        >
+          <Ionicons name="trash" size={20} color="rgb(252, 59, 59)" />
+          <Text style={styles.buttonTextDelete}>Eliminar</Text>
+        </Pressable>
+        <Pressable
+          style={({ pressed }) => [
+            styles.acceptButton,
+            pressed && styles.buttonIsPressed,
+          ]}
+          onPress={() => router.back()}
+          onPressIn={() => setIsPressedA(true)}
+          onPressOut={() => setIsPressedA(false)}
+        >
+          <Ionicons name="checkmark" size={20} color="rgb(81, 196, 71)" />
+          <Text style={styles.buttonTextAccept}>Aceptar</Text>
+        </Pressable>
+      </View>
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { width: '100%', flexGrow: 1, backgroundColor: '#25292e', alignItems: 'center', padding: 20 },
-  image: { width: 300, height: 300, resizeMode: 'contain', marginBottom: 20},
-  title: { fontSize: 20, color: '#fff', fontWeight: 'bold', marginBottom: 10 },
-  text: { fontSize: 16, color: '#fff', marginBottom: 5 },
-  buttonContainer: { width: 300, flexDirection: 'row', flex: 1, justifyContent: 'space-between', alignItems:'flex-end', marginTop: 10, marginBottom: 40 },
-  deleteButton: { flexDirection: 'row', alignItems: 'center', padding: 10, borderColor: 'rgb(252, 59, 59)', borderWidth: 1 },
-  acceptButton: { flexDirection: 'row', alignItems: 'center', padding: 10, borderColor: 'rgb(81, 196, 71)', borderWidth: 1 },
-  buttonTextDelete: { color: 'rgb(252, 59, 59)', marginLeft: 5 },
-  buttonTextAccept: { color: 'rgb(81, 196, 71)', marginLeft: 5 },
-  buttonIsPressed: { backgroundColor: 'rgba(255, 255, 255, 0.1)' },
+  container: {
+    flexGrow: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    padding: 20,
+  },
+  card: {
+    backgroundColor: '#333',
+    borderRadius: 10,
+    padding: 20,
+    width: '100%',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 5,
+    elevation: 5,
+  },
+  image: {
+    width: 300,
+    height: 300,
+    resizeMode: 'contain',
+    marginBottom: 20,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 24,
+    color: '#fff',
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  detailsContainer: {
+    width: '100%',
+  },
+  label: {
+    fontSize: 16,
+    color: '#fff',
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  value: {
+    fontSize: 16,
+    color: '#fff',
+    marginBottom: 15,
+  },
+  buttonContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 20,
+  },
+  deleteButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 15,
+    borderColor: 'rgb(252, 59, 59)',
+    borderWidth: 1,
+    borderRadius: 10,
+    backgroundColor: 'rgba(252, 59, 59, 0.1)',
+  },
+  acceptButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 15,
+    borderColor: 'rgb(81, 196, 71)',
+    borderWidth: 1,
+    borderRadius: 10,
+    backgroundColor: 'rgba(81, 196, 71, 0.1)',
+  },
+  buttonTextDelete: {
+    color: 'rgb(252, 59, 59)',
+    marginLeft: 10,
+    fontWeight: 'bold',
+  },
+  buttonTextAccept: {
+    color: 'rgb(81, 196, 71)',
+    marginLeft: 10,
+    fontWeight: 'bold',
+  },
+  buttonIsPressed: {
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+  },
 });
